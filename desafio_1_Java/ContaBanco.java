66325c42e2ebb87d9bc0e56be5b85057a0c1f16a
krcido <krcido@hotmail.com>
2023-03-22 15:15:12 -0300

    private

--- desafio_1_Java/ContaBanco.java
+++ desafio_1_Java/ContaBanco.java
@@ -2,9 +2,41 @@ package desafio_1_Java;
 
 
 public class ContaBanco {
-	public int numeroConta;
-	public int agencia;
-	public int codigoAgencia;
-	public String nomeCompleto;
-	public double saldoConta;
+	private int numeroConta;
+	private int agencia;
+	private  int codigoAgencia;
+	private  String nomeCompleto;
+	private  double saldoConta;
+	
+	
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	public int getCodigoAgencia() {
+		return codigoAgencia;
+	}
+	public void setCodigoAgencia(int codigoAgencia) {
+		this.codigoAgencia = codigoAgencia;
+	}
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	public double getSaldoConta() {
+		return saldoConta;
+	}
+	public void setSaldoConta(double saldoConta) {
+		this.saldoConta = saldoConta;
+	}	
 }
--- desafio_1_Java/TerminalCliente.java
+++ desafio_1_Java/TerminalCliente.java
@@ -13,27 +13,29 @@ public class TerminalCliente {
 		System.out.println("Bem-vindo ao Banco X");
 		System.out.println("----------------------------------");
 		
+		
 		System.out.println("Digite o número da conta: ");
-		conta1.numeroConta = scanner.nextInt();
+		conta1.setNumeroConta(scanner.nextInt());
 		
 		System.out.println("Digite o número da agência: ");
-		conta1.agencia = scanner.nextInt();
+		conta1.setAgencia (scanner.nextInt());
 		
 		System.out.println("Digite o código da agência: ");
-		conta1.codigoAgencia = scanner.nextInt();
+		conta1.setCodigoAgencia (scanner.nextInt());
 		
 		System.out.println("Digite seu nome completo: ");
-		conta1.nomeCompleto = scanner.next();
+		conta1.setNomeCompleto(scanner.next());
 		
 		System.out.println("Digite o valor para depósito inicial: ");
-		conta1.saldoConta = scanner.nextDouble();
+		conta1.setSaldoConta (scanner.nextDouble());
 		
 		System.out.println("----------------------------------");
 		
-		System.out.println("Olá " + conta1.nomeCompleto + ", obrigado por criar uma conta em nosso banco!" );
-	    System.out.println("Sua agência é " + conta1.agencia + "-" + conta1.codigoAgencia);
-	    System.out.println("Sua conta é " + conta1.numeroConta + " e seu saldo de R$" + conta1.saldoConta + " já está disponível para saque.");
+		System.out.println("Olá " + conta1.getNomeCompleto() + ", obrigado por criar uma conta em nosso banco!" );
+	    System.out.println("Sua agência é " + conta1.getAgencia() + "-" + conta1.getCodigoAgencia());
+	    System.out.println("Sua conta é " + conta1.getNumeroConta() + " e seu saldo de R$" + conta1.getSaldoConta() + " já está disponível para saque.");
 		
-	}
-
+		}
 }
+
+
